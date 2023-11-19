89bc2dacab0f3f2da40252cd9d41d1b407a137db
Luson045 <131430087+Luson045@users.noreply.github.com>
2023-11-19 10:53:10 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+<br/><br/>
+<br/><br/><br/><br/><br/>
+<br/><br/><br/><br/><br/>
+<div class="row gap-1" style="background-color:black;padding:2%;">
+    <div class="cards" style="background-color:black;color:white;">
+    <div class="card-body">
+        <h6>Copyright@23</h6>
+        <a>Follow us on </a><a href="https://www.instagram.com/lusonbasumatary/?hl=en" target="_blank">Instagram</a>
+        <br/>
+        <a href="help.php">Help</a>
+        <br/>
+        <a href="tp.php">Terms & Condition</a>
+    </div>
+</div>
+</div>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,61 @@
-
+<?php
+include_once 'includes/session.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?php 
+            echo "HELLO WORLD-$title";
+        ?>
+    </title>
+    <link href="./css/styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body style="background-color: rgb(69, 138, 141);">
+  <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary" style="border-radius:10px;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+        <img src="images/logo.bmp" alt="Bootstrap" width="50" height="50">
+    </a>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav mr-auto">
+              <?php
+                if(!isset($_SESSION['user_id'])){
+              ?>
+                <li class="nav-item">
+                  <a style="color:orangered;" class="nav-link active" id="head" href="login.php">Sign In</a>
+                </li>
+                <li class="nav-item">
+                    <a style="color:orangered;" class="nav-link active" id="head" href="sign_up.php">Sign Up</a>
+                </li>
+              <?php } else{ ?>
+                <a style="color:orangered;" class="nav-link active" id="head" href=".\interact.php?id=<?php echo $_SESSION['user_id']?>"><span class="sr-only">Hello <?php echo $_SESSION['user_name'] ?>!</span></a>
+                <li class="nav-item">
+                  <a style="color:orangered;" class="nav-link active" id="head" href="logout.php">Sign Out</a>
+                </li>
+              <?php }?>
+        </ul>
+        <li class="nav-item">
+          <a class="nav-link active" style="color:orangered;" id="head" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" style="color:orangered;" id="head" href="courses.php">Explore</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" style="color:orangered;" id="head" href="communicate.php">Communicate</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" style="color:orangered;" id="head" href="help.php">Help</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
